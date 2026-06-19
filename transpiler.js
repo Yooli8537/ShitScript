8e347ae4ba34027df1a7bc3af6be0148584d4d4c
ebholi <uliver@gmx.ch>
2026-06-19 08:30:26 +0200

    Reverted Transpiler to separate checks due to buggyness, added character detection

--- transpiler.js
+++ transpiler.js
@@ -6,29 +6,45 @@ const sourceCode = fs.readFileSync(inputPath, "utf-8");
 let i = 0;
 let WORD = false;
 let NUMBER = false;
+let WHITESPACE = false;
 
 while (i < sourceCode.length) {
   const char = sourceCode[i];
 
-  if (/[a-zA-Z0-9]/.test(char)) {
-    if (/[a-zA-Z]/.test(char)) {
-      WORD = true;
-    } else if (/0-9]/) {
-      NUMBER = true;
-    }
+  if (/[a-zA-Z]/.test(char)) {
     let start = i;
 
-    while (/[a-zA-Z0-9]/.test(sourceCode[i])) {
+    while (/[a-zA-Z]/.test(sourceCode[i])) {
       i++;
     }
 
-    if (WORD) {
-      console.log("WORD: " + sourceCode.slice(start, i));
-      WORD = false;
-    } else if (NUMBER) {
-      console.log("NUMBER: " + sourceCode.slice(start, i));
-      NUMBER = false;
+    console.log("WORD: " + sourceCode.slice(start, i));
+    continue;
+  }
+
+  if (/[0-9]/.test(char)) {
+    let start = i;
+
+    while (/[0-9]/.test(sourceCode[i])) {
+      i++;
     }
+
+    console.log("NUMBER: " + sourceCode.slice(start, i));
+    continue;
+  }
+
+  if (/[\s]/.test(char)) {
+    let start = i;
+
+    while (/[\s]/.test(sourceCode[i])) {
+      i++;
+    }
+    continue;
+  }
+
+  if (/[().]/.test(char)) {
+    console.log("PUNCT: " + sourceCode[i]);
+    i++;
     continue;
   }
   i++;
